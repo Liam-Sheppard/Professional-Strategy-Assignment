6c030408f1c059025004e77d4f49f1f785289c2e
Liam Sheppard <lsheppard1995@gmail.com>
2016-09-21 02:10:29 +1000

    :gem: :lipstick: Navigation has been mostly styled.
    Change in scope. This site is now just design process, no longer full
    documentation.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-      scrollingSpeed: 500,
+      scrollingSpeed: 300,
+      touchSensitivity: 15,
+      normalScrollElementTouchThreshold: 5,
+      anchors: ['sectionone', 'sectiontwo', 'sectionthree', 'sectionfour', 'sectionfive'],
+      slidesNavigation: true,
+      slidesNavPosition: 'bottom'
   });
 });
